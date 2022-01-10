0e6235622f41bad0df287930b1fb4779e533ca16
anuj <anujv962@gmail.com>
2022-01-10 11:31:52 -0800

    last

--- src/App.js
+++ src/App.js
@@ -13,26 +13,12 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 function App() {
   return (
     <>
-      <BrowserRouter>
       <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}>
-          </Route>
-        </Routes>
-        <Routes>
-          <Route path='/cards' element={<CardContainer/>}>
-          </Route>
-        </Routes>
-        <Routes>
-          <Route path='/aboutus' element={<AboutUs/>}>
-          </Route>
-        </Routes>
-        <Routes>
-          <Route path='/services' element={<Services/>}>
-          </Route>
-        </Routes>
-        <LogIn></LogIn>
-      </BrowserRouter>
+      <Home/>
+      <CardContainer/>
+      <AboutUs/>
+      <Services/>
+      <LogIn/>
     </>
   );
 }
